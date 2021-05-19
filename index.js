3cfa3267b453e2ace216143673953592ae3b2511
Krae <kraewind@gmail.com>
2021-05-18 20:43:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+const map = (a, f) => {
+    let r = []
+    for (let i = 0; i < a.length; i++ ) {
+        r.push(f(a[i])) // Unique work
+      }
+    return r
+}
+
+const reduce = (a, s) => {
+  
+}
